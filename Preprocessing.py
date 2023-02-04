0609cf13234247aa9d3c38ff049c3dec59b7eeb2
simsunghyun <ssh3608@naver.com>
2023-02-05 02:24:30 +0900

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,7 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb  4 21:39:04 2023
+import pandas as pd
 
-@author: USER
-"""
-
+class _Preprocessing():
+    
+    def __init__(self, data_name, data_type, time_window, forecasting_term, train_ratio = 0.7):
+        
+        # data_type = univariate time-series data, multivariate time-series data
+        # time_window = input data length 
+        # forecasting_term = forecasting point (ex. 1-step ahead, 3-step ahead)
+        # train_ratio = train / test ratio 
+        
+        self.dt = pd.read_csv('data/' + data_name)
+        self.data_type = data_type 
+        self.time_window = time_window
+        self.forecasting_term = forecasting_term
+        self.train_ratio = train_ratio
+        
+    
+    def preprocessing(self):
+        
+        self.X = self.dt.iloc[:,:-self.forecasting_term]
+        self.Y = self.dt.iloc[:,self.forecasting_term:]
+        
+        
+        
+        
